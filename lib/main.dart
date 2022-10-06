0a6a548865f1260267d7e7f9ed13ed6e275e837b
Saidislom99 <saidislomnajimov@gmail.com>
2022-10-06 22:00:16 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:dio_example/data/local/storage.dart';
+import 'package:dio_example/data/repositories/my_repository.dart';
+import 'package:dio_example/data/services/open_api_provider/open_api_client.dart';
+import 'package:dio_example/data/services/open_api_provider/open_api_provider.dart';
+import 'package:dio_example/presentation/my_home_page.dart';
+import 'package:dio_example/view_models/my_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await StorageRepository.getInstance();
+
+  OpenApiProvider openApiProvider =
+      OpenApiProvider(openApiClient: OpenApiClient());
+
+  runApp(
+      MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => MyViewModel(
+          myRepository: MyRepository(openApiProvider: openApiProvider),
+        ),
+      ),
+      // Provider<OrderViewModel>(
+      //   create: (_) => OrderViewModel(fireStore: fireStore),
+      // ),
+      // Provider<FileViewModel>(
+      //   create: (_) => FileViewModel(),
+      // ),
+      // StreamProvider(
+      //   create: (context) => context.read<AuthViewModel>().authState(),
+      //   initialData: null,
+      // ),
+
+    ],
+    child: MyApp(),
+  )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
